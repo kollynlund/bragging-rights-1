520134d7e3a2d91afb9e0b9ecdf39f95f4699969
kollynlund <kollyn.lund@gmail.com>
2015-01-17 23:56:20 -0700

    nav toggle buttons

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,21 @@
 
 $ ( "#list" ).hide();
 
-$( "#listview" ).click(function() {
+$( "#btn-list" ).click(function() {
   $ ( "#list" ).show();
   $ ( "#map" ).hide();
+  $ ( "#btn-list" ).removeClass( "active" );  
+  $ ( "#btn-list" ).addClass( "btn-list-active" );
+  $ ( "#btn-list" ).addClass( "btn-list-active" );  
+
 });
 
-$( "#mapview" ).click(function() {
+
+$( "#btn-map" ).click(function() {
   $ ( "#map" ).show();
   $ ( "#list" ).hide();
+  $ ( "#btn-map" ).addClass( "btn-map-active" );
+  $ ( "#btn-list" ).removeClass( "btn-list-active" );    
 });
 
 
