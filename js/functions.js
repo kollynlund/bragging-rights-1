28c7ad710fb0cc100e4b6000559db67623cd33a7
kollynlund <kollyn.lund@gmail.com>
2015-01-20 22:59:30 -0700

    added toggle css to the list&map view

--- js/functions.js
+++ js/functions.js
@@ -3,22 +3,23 @@ $ ( "#list" ).hide();
 
 $( "#btn-list" ).click(function() {
   $ ( "#list" ).show();
-  $ ( "#map" ).hide();
-  $ ( "#btn-list" ).removeClass( "active" );  
-  $ ( "#btn-list" ).addClass( "btn-list-active" );
-  $ ( "#btn-list" ).addClass( "btn-list-active" );  
-
+  $ ( "#map" ).hide();  
 });
 
 
 $( "#btn-map" ).click(function() {
   $ ( "#map" ).show();
   $ ( "#list" ).hide();
-  $ ( "#btn-map" ).addClass( "btn-map-active" );
-  $ ( "#btn-list" ).removeClass( "btn-list-active" );    
 });
 
 
+$( "#btn-map" ).click(function() {
+  $( this ).toggleClass( "btn-toggle-active" );
+  $( "#btn-list" ).removeClass( "btn-toggle-active" )
+});
 
-
+$( "#btn-list" ).click(function() {
+  $( this ).toggleClass( "btn-toggle-active" );
+  $( "#btn-map" ).removeClass( "btn-toggle-active" )
+});
 
