b6a3c02cd15d377167f25901dbbc64d93b5965e5
kollynlund <kollyn.lund@gmail.com>
2015-01-22 15:59:49 -0700

    lots of css changes. BE prepared!!!!!!!!!!!

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,6 @@
 
+// SHOW/HIDE MAP WHEN BUTTON CLICKED
+
 $ ( "#list" ).hide();
 
 $( "#btn-list" ).click(function() {
@@ -13,13 +15,39 @@ $( "#btn-map" ).click(function() {
 });
 
 
+// TOGGLE STYLES FOR MAP/LIST BUTTONS
+
 $( "#btn-map" ).click(function() {
-  $( this ).toggleClass( "btn-toggle-active" );
+  $( this ).addClass( "btn-toggle-active" );
   $( "#btn-list" ).removeClass( "btn-toggle-active" )
 });
 
 $( "#btn-list" ).click(function() {
-  $( this ).toggleClass( "btn-toggle-active" );
+  $( this ).addClass( "btn-toggle-active" );
   $( "#btn-map" ).removeClass( "btn-toggle-active" )
 });
 
+// TOGGLE STYLES FOR MAP TOOLS
+
+$( ".fa-search-minus" ).click(function() {
+  $( this ).addClass( "btn-toggle-active" );
+  $( ".fa-search-plus" ).removeClass( "btn-toggle-active" )
+  $( ".glyphicon-move" ).removeClass( "btn-toggle-active" )
+  $( "#map-container:hover" ).css( "cursor", "-webkit-zoom-out" )
+});
+
+$( ".fa-search-plus" ).click(function() {
+  $( this ).addClass( "btn-toggle-active" );
+  $( ".fa-search-minus" ).removeClass( "btn-toggle-active" )
+  $( ".glyphicon-move" ).removeClass( "btn-toggle-active" )
+  $( "#map-container:hover" ).css( "cursor", "-webkit-zoom-in" )
+
+});
+
+$( ".glyphicon-move" ).click(function() {
+  $( this ).addClass( "btn-toggle-active" );
+  $( ".fa-search-plus" ).removeClass( "btn-toggle-active" )
+  $( ".fa-search-minus" ).removeClass( "btn-toggle-active" )
+  $( "#map-container:hover" ).css( "cursor", "move" )
+
+});
